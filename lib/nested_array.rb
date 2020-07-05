33b00c28f4115a96e627a7b182e9988b02969062
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-05 16:21:37 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -33,8 +33,7 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
-  matrix[0,1]
-  CONVENTIONAL_PRODUCE(0,1)
+  CONVENTIONAL_PRODUCE[0,1]
   
 end
 
